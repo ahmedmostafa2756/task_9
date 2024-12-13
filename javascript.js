2726ffa62a393b4a457d1d132966d285f8e82343
ahmed mostafa <moah2588@gmail.com>
2024-12-13 16:32:16 +0200

    task_8

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var nam1=+ prompt("Enter The First Number")
+var nam2=+ prompt("Enter The Second Number")
+var sum = nam1+nam2
+document.write(" the sum =" + sum + "<br>"  )
+
+
+
+var sub = nam1-nam2
+document.write( " the sub =" + sub + "<br>" )
+
+
+var multi = nam1*nam2
+document.write(" the multiplcation =" + multi + "<br>"  )
+
+
+
+
+var devi = nam1/nam2
+document.write(" the divsion =" + devi + "<br>"  )
+
+var modu = nam1%nam2
+document.write(" the modulus  =" + devi + "<br>"  )
