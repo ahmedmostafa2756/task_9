6d37dc1e98488158b2cf4546aec2fa7ecf72ef7c
ahmedmostafa2756 <moah2588@gmail.com>
2024-12-25 21:34:57 +0200

    task9

--- javascript.js
+++ javascript.js
@@ -1,22 +1,90 @@
-var nam1=+ prompt("Enter The First Number")
-var nam2=+ prompt("Enter The Second Number")
-var sum = nam1+nam2
-document.write(" the sum =" + sum + "<br>"  )
+var nam1 = + prompt("Enter The First Number")
+var nam2 = + prompt("Enter The Second Number")
+
+var sum = nam1 + nam2
 
 
+document.write(" the sum =" + sum + "<br>")
 
-var sub = nam1-nam2
-document.write( " the sub =" + sub + "<br>" )
+var nam = + prompt("Enter The   Number")
+var modu = nam % 2
+if (modu == 0) {
+    document.write(" the number is even" + "<br>");
+} else {
+    document.write(" the number is odd" + "<br>");
+}
+
+if (nam1 > nam2) {
+    document.write("nam1 is the max" + "<br>");
+}
+else if (nam1 < nam2)  {
+    document.write("nam2 is the max" + "<br>");
+}
+else{
+    document.write("2 number is equel" + "<br>");
+}
+var month = + prompt("Enter The month")
+var day
+switch (month) {
+    case 1:
+        day = 31;
+        break;
+    case 2:
+        day = 28;
+        break;
+    case 3:
+        day = 31;
+        break;
+    case 4:
+        day = 30;
+        break;
+    case 5:
+        day = 31;
+        break;
+    case 6:
+        day = 30;
+        break;
+    case 7:
+        day = 31;
+        break;
+    case 8:
+        day = 31;
+        break;
+    case 9:
+        day = 30;
+        break;
+    case 10:
+        day = 31;
+        break;
+    case 11:
+        day = 30;
+        break;
+    case 12:
+        day = 31;
+        break;
 
 
-var multi = nam1*nam2
-document.write(" the multiplcation =" + multi + "<br>"  )
+}
+document.write("Number of days   " + day + "<br>");
 
+var letter1 = prompt("Enter The  letter")
+switch (letter1) {
+    case 'a':
+    case 'o':
+    case 'e':
+    case 'i':
+    case 'u':
+    case 'A':
+    case 'O':
+    case 'E':
+    case 'I':
+    case 'U':
 
+        document.write("character vowel " + "<br>");
 
+        break;
+    default:
+        document.write("character constant " + "<br>");
 
-var devi = nam1/nam2
-document.write(" the divsion =" + devi + "<br>"  )
+}
 
-var modu = nam1%nam2
-document.write(" the modulus  =" + devi + "<br>"  )
